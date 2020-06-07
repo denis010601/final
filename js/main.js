5b57eecebd1cd675d6a95e476eb057f3bb86bdfb
denis010601 <57405778+denis010601@users.noreply.github.com>
2020-06-08 03:01:41 +0400

    + functional 1

--- js/main.js
+++ js/main.js
@@ -15,13 +15,15 @@ closeBtn.on('click', function (){
 modal.toggleClass('modal--visible');
 });
 thank.on('click', function () {
-thank.revomeClass('thank--visible');
+thank.removeClass('thank--visible');
 });
+
 $('.modal').click(function(e) {
   if ($(e.target).closest('.modal__dialog').length == 0) {
-    modal.toggleClass('modal--visible');				
+    modal.removeClass('modal--visible');				
   }
 });	
+
 
 
 
@@ -105,6 +107,188 @@ var swiper = new Swiper('.reviews .swiper-container', {
     });
     $('[type=tel]').mask('+7(000) 000-00-00', {placeholder: "Телефон обязателен"});
 
+    
+    $(".pin__form").validate({
+      errorElement: "div",
+      errorClass: "invalid",
+      rules: {
+        // simple rule, converted to {required:true}
+        userName: {
+          required: true,
+          minlength: 2,
+          maxlength:15
+        },
+        userPhone: "required",
+        // compound rule
+        userEmail: {
+          required: true,
+          email: true
+        },
+        checkbox: {
+          required : true
+        }
+        
+      },
+        messages: {
+          userName: {
+            required:"Имя обязательно",
+            minlength: "Имя не короче двух букв",
+            maxlength: "Имя не длиньше 15 букв"
+
+          } ,
+          userPhone: "Телефон обязателен",
+          userEmail: {
+            required: "Укажите email",
+            email: "введите формате: name@domain.com"
+          },
+        checkbox: "Примите соглашение"
+    },
+    submitHandler: function(form) {
+      $.ajax ({
+        type: "POST",
+        url: "send.php",
+        data: $(form).serialize(),
+        success: function (response) {
+          thank.toggleClass('thank--visible');
+          $(form)[0].reset();
+          modal.removeClass('modal--visible');
+          
+        }
+      });
+    }
+
+    });
+
+    $(".bid__form").validate({
+      errorElement: "div",
+      errorClass: "invalid",
+      rules: {
+        // simple rule, converted to {required:true}
+        userName: {
+          required: true,
+          minlength: 2,
+          maxlength:15
+        },
+        userPhone: "required",
+        // compound rule
+        userEmail: {
+          required: true,
+          email: true
+        },
+        checkbox: {
+          required : true
+        }
+        
+      },
+        messages: {
+          userName: {
+            required:"Имя обязательно",
+            minlength: "Имя не короче двух букв",
+            maxlength: "Имя не длиньше 15 букв"
+
+          } ,
+          userPhone: "Телефон обязателен",
+          userEmail: {
+            required: "Укажите email",
+            email: "введите формате: name@domain.com"
+          },
+        checkbox: "Примите соглашение"
+    },
+    submitHandler: function(form) {
+      $.ajax ({
+        type: "POST",
+        url: "send.php",
+        data: $(form).serialize(),
+        success: function (response) {
+          thank.toggleClass('thank--visible');
+          $(form)[0].reset();
+          modal.removeClass('modal--visible');
+        }
+      });
+    }
+    });
+    
+
+    var player;
+  $('.way .video__play').on('click', function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+      height: '395',
+      width: '100%',
+      videoId: 'O321R3JvCpM',
+      events: {
+        'onReady': videoPlay,
+      }
+    });
+  })
+
+  function videoPlay(event){
+    event.target.playVideo();
+  }
 
 
-})
+  var player;
+  $('.video__play-1').on('click', function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player1', {
+      height: '395',
+      width: '100%',
+      videoId: 'O321R3JvCpM',
+      events: {
+        'onReady': videoPlay,
+      }
+    });
+  })
+
+  function videoPlay(event){
+    event.target.playVideo();
+  }
+
+  var player;
+  $('.video__play-2').on('click', function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player2', {
+      height: '395',
+      width: '100%',
+      videoId: 'O321R3JvCpM',
+      events: {
+        'onReady': videoPlay,
+      }
+    });
+  })
+
+  function videoPlay(event){
+    event.target.playVideo();
+  }
+
+  var player;
+  $('.video__play-3').on('click', function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player3', {
+      height: '395',
+      width: '100%',
+      videoId: 'O321R3JvCpM',
+      events: {
+        'onReady': videoPlay,
+      }
+    });
+  })
+
+  function videoPlay(event){
+    event.target.playVideo();
+  }
+  
+});
+
+document.addEventListener ('DOMContentLoaded', function (){
+  const links = document.querySelectorAll ('.menu__link');
+
+  for(let i = 0; i<links.length; i++){
+    links[i].addEventListener('click', function (event) {
+      event.preventDefault();
+      
+      const blockID = event.target.getAttribute("href").substr(1);
+
+      document.getElementById(blockID).scrollIntoView({
+        behavior: 'smooth',
+        block: "start"
+      })
+    })
+  }
+});
